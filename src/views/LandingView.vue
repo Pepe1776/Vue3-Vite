ad0be431772d08c13687cddd13f6ec2306964203
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-06-24 21:55:31 -0700

    added pic

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -275,6 +275,16 @@
       </div>
       <img
         data-aos="flip-down"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        data-aos-mirror="true"
+        data-aos-once="false"
+        data-aos-anchor-placement="top-center"
+        class="frame"
+        src="../assets/Bees-Knees.png"
+      />
+      <img
+        data-aos="flip-down"
         data-aos-offset="200"
         data-aos-delay="50"
         data-aos-duration="1000"
